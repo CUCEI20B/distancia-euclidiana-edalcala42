637304ac2244fa4f363cb52b9d92b1e3792f7998
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-10-12 16:57:02 -0500

    Se sustituyó el signo de resta por el de suma dentro de la fórmula

--- algoritmos.py
+++ algoritmos.py
@@ -3,8 +3,5 @@ import math
 def distancia_euclidiana(x_1, y_1, x_2, y_2)->float:
     x = x_2 - x_1
     y = y_2 - y_1
-
-    return (math.sqrt((x*x)-(y*y)))
-
-
-    
+    return (math.sqrt((x*x)+(y*y)))
+  
